3c032ccb19d7788c5798f929ecd2ebb409e1b25b
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-08 22:05:05 +0000

    Implementation of eqArrays.js for array comparision

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+//Function assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Function that compares arrays
+const eqArrays = function(arr1, arr2) {
+  let arrLength = arr1.length;
+  
+  for (let i = 0; i < arrLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+//Testing the code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
