71c456891618142b82ac3aa69ef0fda145571157
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-08 23:36:33 +0000

    eqArrays updated to identify arrays length and assertArrausEquals implemented

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,12 @@ const assertEqual = function(actual, expected) {
 
 //Function that compares arrays
 const eqArrays = function(arr1, arr2) {
-  let arrLength = arr1.length;
+  let arrLength = 0;
+  if (arr1.length === arr2.length) {
+    arrLength = arr1.length;
+  } else {
+    return false;
+  }
   
   for (let i = 0; i < arrLength; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -25,3 +30,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
