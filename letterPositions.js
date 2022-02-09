f9bcd3f14c9a4075cd844d04c8ddb9f45e53096c
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-09 20:29:18 +0000

    implementing letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+//Function that compares arrays
+const eqArrays = function(arr1, arr2) {
+  let arrLength = 0;
+  if (arr1.length === arr2.length) {
+    arrLength = arr1.length;
+  } else {
+    return false;
+  }
+  
+  for (let i = 0; i < arrLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//Function that compares arrays
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [];
+      results[sentence[i]].push(i);
+    }
+  }
+
+  return results;
+};
+
+//Test code
+assertArraysEqual(letterPositions("hello").e, [1]);
