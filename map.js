a25ee937193a597eebc13c821be565243cc7497a
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-02-10 19:20:26 +0000

    implement map.js

---
+++ map.js
@@ -0,0 +1,46 @@
+//Function that compares arrays
+const eqArrays = function(arr1, arr2) {
+  let arrLength = 0;
+  if (arr1.length === arr2.length) {
+    arrLength = arr1.length;
+  } else {
+    return false;
+  }
+  
+  for (let i = 0; i < arrLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//function to test the code
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+//implement map
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//TEST code
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [ 6, 7, 2, 5, 3 ]);
+
+const results3 = map(words, word => word.toUpperCase());
+assertArraysEqual(results3, [ 'GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM' ]);
